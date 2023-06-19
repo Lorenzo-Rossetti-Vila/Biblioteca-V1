061ae6cd033f2980b9f8598c9f3fd1f1338210cd
Lorenzo-Rossetti-Vila <110860468+Lorenzo-Rossetti-Vila@users.noreply.github.com>
2023-06-19 19:55:54 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAD-s_MrBThg3yiI3G5KerpKaf9zIPe1kE",
+  authDomain: "c-58-c59e9.firebaseapp.com",
+  databaseURL: "https://c-58-c59e9-default-rtdb.firebaseio.com",
+  projectId: "c-58-c59e9",
+  storageBucket: "c-58-c59e9.appspot.com",
+  messagingSenderId: "299677044510",
+  appId: "1:299677044510:web:3457695285abe37969c30f"
+};
+
